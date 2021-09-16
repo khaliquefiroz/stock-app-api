db1ec1016a10c67fa9d1e4deca4d3643b4b817f5
khaliquefiroz <khaliquefiroz@gmail.com>
2021-09-16 08:27:18 +0530

    server setup

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const morgan = require('morgan');
-const express = require('express');
-const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 console.log(__dirname);
 
-app.post('/api/users', (req, res)=> {
-	res.status(200).send('started');
-})
+app.post("/api/users", (req, res) => {
+  res.status(200).send("started");
+});
+app.get("/api/welcome", (req, res) => {
+  res.status(200).json({
+    message: "successful",
+    status: "okay",
+  });
+});
 
 module.exports = app;
